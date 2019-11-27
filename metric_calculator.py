f94570cfff1b3a03b190f5b869f78b4f984e4715
Haoyang_Liu96 <haoyangliu96@gmail.com>
2019-11-26 21:21:00 -0500

    add an accuracy calculator for both emoji are correctly predicted.

---
+++ metric_calculator.py
@@ -0,0 +1,24 @@
+def overall_accuracy(test_target, test_prediction):
+    correct_prediction_count = 0
+    for target, prediction in zip(test_target, test_prediction):
+        if prediction[0] == prediction[1]:
+            if target[0] == target[1]:
+                if prediction[0] in target:
+                    correct_prediction_count += 2
+            else:
+                if prediction[0] in target:
+                    correct_prediction_count += 1
+        else:
+            for p in prediction:
+                if p in target:
+                    correct_prediction_count += 1
+    return correct_prediction_count / (2 * len(test_target))
+
+def both_emoji_accuracy(test_target, test_prediction):
+    correct_prediction_count = 0
+    for target, prediction in zip(test_target, test_prediction):
+        if prediction[0] == prediction[1]:
+            if target[0] == target[1]:
+                if prediction[0] in target:
+                    correct_prediction_count += 2
+    return correct_prediction_count / (2 * len(test_target))
