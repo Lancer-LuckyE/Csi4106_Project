f995ff8446072a438f94a71245e95af65509e252
WeihaoH <whuan028@uottawa.ca>
2019-12-04 16:10:16 -0500

    Revert "Merge branch 'master' of https://github.com/Lancer-LuckyE/Csi4106_Project"
    This reverts commit 170b6ff01e954d6331d5e17a38d33722978c77f5, reversing
    changes made to b6ea083fb0b32304a6dc7166a3fe248d79b4562a.

--- preprocess.py
+++ preprocess.py
@@ -15,51 +15,53 @@ def contain_meaningful_text(text_preprocessing_result):
     return len(text_preprocessing_result) != 0
 
 
-# The line to start from
-# start from (start_point+1)th line
-def preprocess(amount_to_be_preprocessed, start_point=0):
+def preprocess(start_point, amount_to_be_preprocessed):
     # The file to be processed
     target = 'data/emojitweets-01-04-2018.txt'
+
+    # The line to start from
+    # start from (start_point+1)th line
 
     wnl = nltk.WordNetLemmatizer()
 
     start_time = process_time()
     with open(target, 'r', encoding='utf-8') as f:
-
         pointer = 0
-        counter = 0 # count of successfully generated data
-        with open('data/TEST_OUTPUT_%s.csv' % amount_to_be_preprocessed, 'w', encoding='utf-8', newline='') as o:
+        with open('data/TEST_OUTPUT_%s.csv'%amount_to_be_preprocessed, 'w', encoding='utf-8') as o:
             writer = csv.writer(o)
-            for line in f:
+            writer.writerow(["text", "emoji_1", "emoji_2"])
+            for i, line in enumerate(f):
 
                 if pointer < start_point:
                     pointer += 1
                     continue
 
-                if counter == amount_to_be_preprocessed:
+                if i == amount_to_be_preprocessed:
                     break
 
                 emoji_extraction_result = extract_emoji(line)
 
                 # Examine if there is any emoji in text
                 if contain_emoji(emoji_extraction_result):
+                    try:
+                        raw_text = emoji_extraction_result[0]
 
-                    raw_text = emoji_extraction_result[0]
+                        # Preprocess text
+                        preprocessed_text = preprocess_text(wnl, raw_text)
 
-                    # Preprocess text
-                    preprocessed_text = preprocess_text(wnl, raw_text)
-
-                    # Processed text may be '', which means that there is no meaningful information in this text.
-                    # We therefore do not put this entry in our result.
-                    if contain_meaningful_text(preprocessed_text):
-                        writer.writerow(
-                            [preprocessed_text, emoji_extraction_result[1][0], emoji_extraction_result[1][1]])
-                        counter += 1
-
+                        # Processed text may be '', which means that there is no meaningful information in this text.
+                        # We therefore do not put this entry in our result.
+                        if contain_meaningful_text(preprocessed_text):
+                            writer.writerow(
+                                [preprocessed_text, emoji_extraction_result[1][0], emoji_extraction_result[1][1]])
+                            print("line " + str(i + 1) + "is done.")
+                    except Exception as e:
+                        print(e)
                 pointer += 1
     finish_time = process_time()
     print('Done! Processed %s records in %s seconds.' % (amount_to_be_preprocessed, round(finish_time - start_time, 2)))
 
 
-if __name__ == '__main__':
-    preprocess(round(3e5), 0)
+if __name__ == "__main__":
+    preprocess(0, 200000)
+
