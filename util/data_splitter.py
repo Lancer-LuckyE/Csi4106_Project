ca5a640c28f4502006b1424ffe675b06cd8b7ded
marcmk6 <31750443+marcmk6@users.noreply.github.com>
2019-12-03 23:25:54 -0500

    fix path

--- util/data_splitter.py
+++ util/data_splitter.py
@@ -23,33 +23,32 @@ def split_dataset(txt_path, emoji_path, validation=False):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=10)
 
-    with open('data/t2e_train.text', 'w') as train:
+    with open('../data/t2e_train.text', 'w') as train:
         for line in X_train:
             train.write(line)
-    with open('data/t2e_train.emoji', 'w') as train:
+    with open('../data/t2e_train.emoji', 'w') as train:
         for line in y_train:
             train.write(line)
 
     if validation:
         X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5,
                                                                       random_state=10)
-        with open('data/t2e_validation.text', 'w') as validation:
+        with open('../data/t2e_validation.text', 'w') as validation:
             for line in X_validation:
                 validation.write(line)
-        with open('data/t2e_validation.emoji', 'w') as validation:
+        with open('../data/t2e_validation.emoji', 'w') as validation:
             for line in y_validation:
                 validation.write(line)
 
-    with open('data/t2e_test.text', 'w') as test:
+    with open('../data/t2e_test.text', 'w') as test:
         for line in X_test:
             test.write(line)
-    with open('data/t2e_test.emoji', 'w') as test:
+    with open('../data/t2e_test.emoji', 'w') as test:
         for line in y_test:
             test.write(line)
 
 
 if __name__ == "__main__":
-    path = 'data/TEST_OUTPUT.csv'
+    path = '../data/TEST_OUTPUT_200000.csv'
     csv2dataset(path)
-
-    split_dataset('data/all_data.text', 'data/all_data.emoji', validation=False)
+    split_dataset('../data/all_data.text', '../data/all_data.emoji', validation=False)
