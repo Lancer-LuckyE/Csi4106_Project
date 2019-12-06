7ebcc44a9ef592858812820b3eb90bb00f34d7d6
marcmk6 <31750443+marcmk6@users.noreply.github.com>
2019-12-06 16:37:40 -0500

    recover

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,6 @@ import nltk
 from time import process_time
 from emoji_extractor import extract_emoji
 from text_preprocessor import preprocess_text
-import multiprocessing as mp
 
 
 def contain_emoji(emoji_extraction_result):
@@ -29,14 +28,17 @@ def preprocess(amount_to_be_preprocessed, start_point=0):
 
         pointer = 0
         counter = 0 # count of successfully generated data
-        with open('data/TEST_OUTPUT.csv', 'a+', encoding='utf-8') as o:
+        with open('data/TEST_OUTPUT_%s.csv' % amount_to_be_preprocessed, 'w', encoding='utf-8', newline='') as o:
             writer = csv.writer(o)
             for line in f:
+
                 if pointer < start_point:
                     pointer += 1
                     continue
+
                 if counter == amount_to_be_preprocessed:
                     break
+
                 emoji_extraction_result = extract_emoji(line)
 
                 # Examine if there is any emoji in text
@@ -53,34 +55,11 @@ def preprocess(amount_to_be_preprocessed, start_point=0):
                         writer.writerow(
                             [preprocessed_text, emoji_extraction_result[1][0], emoji_extraction_result[1][1]])
                         counter += 1
+
                 pointer += 1
-                print('The ' + str(pointer) + ' line is done.')
     finish_time = process_time()
     print('Done! Processed %s records in %s seconds.' % (amount_to_be_preprocessed, round(finish_time - start_time, 2)))
 
 
 if __name__ == '__main__':
-    # Create 6 processes to run parallely 
-    p1 = mp.Process(target=preprocess, args=(50000, 0))         # 0 ~ 49999
-    p2 = mp.Process(target=preprocess, args=(50000, 50000))     # 50000 ~ 99999
-    p3 = mp.Process(target=preprocess, args=(50000, 100000))    # 100000 ~ 149999
-    p4 = mp.Process(target=preprocess, args=(50000, 150000))    # 150000 ~ 199999
-    p5 = mp.Process(target=preprocess, args=(50000, 200000))    # 200000 ~ 249999
-    p6 = mp.Process(target=preprocess, args=(50000, 250000))    # 250000 ~ 299999
-    
-    # processes start
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    
-    # join the result
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    print("done")
+    preprocess(round(3e5), 0)
