1cdcb2953b23c92846ef50541ee3b4634a93c1f7
Haoyang_Liu96 <haoyangliu96@gmail.com>
2019-12-05 19:36:52 -0500

    clean up the low frequency classes feature. spelling correction feature.

--- text_preprocessor.py
+++ text_preprocessor.py
@@ -2,10 +2,36 @@ import nltk
 import re
 from nltk.corpus import wordnet
 from nltk.corpus import stopwords
+from spellchecker import SpellChecker
 
 '''
 Used for removing meaningless information from text.
 '''
+
+
+def reduce_reqeated_letters(token):
+    reduced_token = token[0]
+    i = 1
+    repeat_count = 1
+    while i < len(token):
+        print(i)
+        if token[i] != reduced_token[-1]:
+            reduced_token += token[i]
+            repeat_count = 1
+            i += 1
+        elif token[i] == reduced_token[-1] and repeat_count < 2:
+            reduced_token += token[i]
+            repeat_count += 1
+            i += 1
+        else:
+            repeat_count += 1
+            i += 1
+    return reduced_token
+
+
+def reduce_repeat(token):
+    pattern = re.compile(r"(.)\1{2,}")
+    return pattern.sub(r"\1\1", token)
 
 
 def get_wordnet_pos(treebank_tag):
@@ -23,6 +49,7 @@ def get_wordnet_pos(treebank_tag):
 
 def preprocess_text(word_net_lemmatizer, single_text_entry):
     sentences = nltk.sent_tokenize(single_text_entry)
+    spell = SpellChecker()
 
     processed_sentences = ''
 
@@ -35,6 +62,10 @@ def preprocess_text(word_net_lemmatizer, single_text_entry):
 
         # Remove stopwords
         tokens = [t for t in tokens if t not in stopwords.words('english')]
+
+        # Correction spelling
+        for i in range(len(tokens)):
+            tokens[i] = spell.correction(reduce_repeat(tokens[i]))
 
         # POS Tagging
         pos_tokens = nltk.pos_tag(tokens)
